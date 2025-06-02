c31f9580fe6eb11010991ef850e73c5d1feba945
vicEduDev <victoreduardo2203@gmail.com>
2025-06-02 08:12:26 -0300

    First commit

---
+++ helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    //Just testing some c++ features
+    /*
+        And some of them are the comments :)
+    */
+    std::cout << "Hello world... Yeah, I guess that's it" << '\n';
+    std::cout << "Did I jump the line?" << '\n';
+
+    //Let's declare some variables
+    int x; //Yeah, we've declared
+    x = 5; //Now, we've given it some value
+    std::cout << x;
+
+    return 0;
+}
