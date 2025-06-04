d31f9855eb272e563507a381ae39d9d78d394b7b
vicEduDev <victoreduardo2203@gmail.com>
2025-06-03 21:57:05 -0300

    Variables types (and strings)

--- helloworld.cpp
+++ helloworld.cpp
@@ -9,9 +9,28 @@ int main(){
     std::cout << "Did I jump the line?" << '\n';
 
     //Let's declare some variables
+    //Int variables
     int x; //Yeah, we've declared
     x = 5; //Now, we've given it some value
-    std::cout << x;
+    std::cout << x << '\n';
+
+    //Dobule variable
+    double dec_num = 1.5;
+    double dec_num_yes = 2.345;
+    std::cout << dec_num << '\n';
+    std::cout << dec_num_yes << '\n';
+
+    //Single Character variable
+    char letter = 'V';
+    std::cout << letter << '\n';
+    
+    //Boolean variable
+    bool state = false;
+    std::cout << state << '\n';
+
+    //String 'variable' (It's technically an object)
+    std::string name = "Victor";
+    std::cout << name << '\n';
 
     return 0;
 }
